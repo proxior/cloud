c4a3a2fa1b0d6ccdbbe41a901d589f780f1a57e1
mak <makindosx@gmail.com>
2020-02-28 01:43:41 +0200

    Add files via upload

--- logout.php
+++ logout.php
@@ -21,9 +21,9 @@
 
 session_start();
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
 
 require('__ROOT__/class_cn.php');
 require('browser_user.php');
